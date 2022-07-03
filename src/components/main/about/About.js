239c1aeaa8063f419456be436d8e786204e375fd
Snozin <msl.snzn@gmail.com>
2022-07-03 14:14:10 +0100

    Versión inicial de sección Sobre mi

--- src/components/main/about/About.js
+++ src/components/main/about/About.js
@@ -1,9 +1,51 @@
-import { AboutContainer } from "./styledAbout";
+import {
+  AboutContainer,
+  Bio,
+  Title,
+  Highlight,
+  Img,
+  ImgContainer,
+  InfoContainer,
+  Name,
+  SkillsContainer,
+  SkillTags,
+} from './styledAbout'
+import { Tag } from '../../../styles/globalStyles'
+import profile from '../../../assets/img/profile-small.jpg'
 
 export default function Hero() {
   return (
     <AboutContainer>
-      About Me Section
+      <ImgContainer>
+        <Img src={profile} />
+      </ImgContainer>
+      <InfoContainer>
+        <Title>Encantado de conocerte,</Title>
+        <Name>soy Misael</Name>
+        <Bio>
+          Texto muy chachi molón sobre como molo y lo mucho que molo, porque a
+          ver, seamos serios, molar molo.
+        </Bio>
+        <Highlight>Codeación Intensa Duramente.</Highlight>
+        <Bio>
+          Habrá que poner algo aquí para que se vea que molo sin que sea muy
+          sobrado para no molestar al resto de mortales que puede que molen
+          también pero no tanto como molo yo.
+        </Bio>
+      </InfoContainer>
+
+      <SkillsContainer>
+        <Title style={{ 'padding-bottom': 8 }}>Tengo experiencia con </Title>
+        <SkillTags>
+          <Tag>JavaScript</Tag>
+          <Tag>React</Tag>
+          <Tag>NodeJS</Tag>
+          <Tag>Express</Tag>
+          <Tag>MongoDB</Tag>
+          <Tag>HTML</Tag>
+          <Tag>CSS</Tag>
+        </SkillTags>
+      </SkillsContainer>
     </AboutContainer>
   )
 }
--- src/components/main/about/styledAbout.js
+++ src/components/main/about/styledAbout.js
@@ -2,5 +2,62 @@ import styled from 'styled-components'
 import { Section } from '../../../styles/globalStyles'
 
 export const AboutContainer = styled(Section)`
-  /* background-color: aquamarine; */
+  display: flex;
+  flex-direction: column;
+  /* background-color: #EEEEEE; */
+`
+
+export const ImgContainer = styled.div`
+  width: 100%;
+  height: 260px;
+  overflow: hidden;
+  border-radius: 10px;
+`
+
+export const Img = styled.img`
+  width: inherit;
+  height: inherit;
+  object-fit: cover;
+`
+
+export const InfoContainer = styled.div`
+  padding: 16px 0;
+`
+
+export const Title = styled.h2`
+  font-size: 20px;
+  line-height: 28px;
+  letter-spacing: -0.02em;
+`
+
+export const Name = styled.h3`
+  color: var(--primary-color);
+  padding-bottom: 12px;
+  font-size: 24px;
+  font-family: 'Inter-Bold';
+  line-height: 32px;
+  letter-spacing: -0.02em;
+`
+
+export const Bio = styled.p`
+  font-size: 16px;
+  line-height: 24px;
+`
+
+export const Highlight = styled.p`
+  color: var(--primary-color);
+  padding: 12px 0;
+  font-size: 20px;
+  font-family: 'Helvetica';
+  line-height: 26px;
+`
+
+export const SkillsContainer = styled.div`
+  /* background-color: aqua; */
+`
+
+export const SkillTags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
 `
