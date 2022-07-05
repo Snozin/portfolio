039843a837ac0f890d95dbd769e9a6fd968b569e
Snozin <msl.snzn@gmail.com>
2022-07-05 16:21:32 +0100

    Agregado Favicon y limpieza de boilerplate

--- src/components/main/contact/styledContact.js
+++ src/components/main/contact/styledContact.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 import { Section } from '../../../styles/globalStyles'
 
 export const ContactContainer = styled(Section)`
+  padding-bottom: 50px;
+
   @media screen and (min-width: 1024px) {
     max-width: 550px;
     width: 550px;
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -38,6 +38,8 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
+    margin: 0;
+
     font-family: 'Inter';
     color: var(--text-color);
     background-color: var(--bg-color);
